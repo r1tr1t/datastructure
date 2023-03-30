11e0c7d8d80a791c6b2d31c9f62247de6ec54a94
Eshnek <dylanpozarnsky@gmail.com>
2023-03-29 22:02:54 -0600

    3/29/2023 22:02 +

--- src/DList.hxx
+++ src/DList.hxx
@@ -1,8 +1,21 @@
 #pragma once
 
+template<typename T>
+struct Node {
+    Node* last {};
+    Node* next {};
+
+    T data {};
+};
+
 template< typename T>
 class DList
 {
+    public:
+
+    Node<T>* first {};
+    Node<T>* last {};
+
     public:
 
     DList () {
