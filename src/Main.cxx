263205306a36cf45f1e10df84cddea46603066cb
Eshnek <dylanpozarnsky@gmail.com>
2023-03-29 21:59:28 -0600

    3/29/2023 21:59 +

--- src/Main.cxx
+++ src/Main.cxx
@@ -1,28 +1,6 @@
-#include "Object.hxx"
+#include "DList.hxx"
 
 int main ()
 {
-    // : Instantiate a new instance of "Object"
-    Object o {};
-
-    // : Print the memory address of object "o"
-    std::cout << &o << "\n";
-
     return 0;
-}
-
-//
-
-// declare function a
-void a();
-// declare function b
-void b();
-
-// define function a
-void a() {
-    b();
-}
-// define function b
-void b() {
-
 }
